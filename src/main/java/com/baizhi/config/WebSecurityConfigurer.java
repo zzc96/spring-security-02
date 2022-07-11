4ac50deb341f0def39a024ed3363485e7c0a3ccc
zzc96 <978009984@qq.com>
2022-07-11 22:22:23 +0800

    init

---
+++ src/main/java/com/baizhi/config/WebSecurityConfigurer.java
@@ -0,0 +1,24 @@
+package com.baizhi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/login.html").permitAll()
+                .mvcMatchers("/index").permitAll()  // 放行资源写在前面
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/doLogin")
+                .usernameParameter("uname")
+                .usernameParameter("passwd")
+                .and()
+                .csrf().disable();
+    }
+}
